e75bfdb0316934b63115bfd368086147b25b3e9e
yangjay <yj960203only@gmail.com>
2019-06-02 23:56:31 +0800

    feat(component): lc-button 组件增加icon,并可设置icon位置

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,30 +1,71 @@
 <template>
-     <button class="lc-button"> 确定 </button>
+  <button class="lc-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-    name: '',
-}
+  name: "",
+  props: {
+    icon: {
+      type: String
+    },
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator (value) {
+        return ['left', 'right'].includes(value)
+      }
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
-.lc-button{
-    font-size: var(--button-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover{
+.lc-button {
+  font-size: var(--button-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
     border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .content {
+      order: 1;
     }
-    &:active{
-        background-color: var(--button-active-bg);
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
     }
-    &:focus{
-        outline: none;
-    }
+  }
 }
 </style>
 

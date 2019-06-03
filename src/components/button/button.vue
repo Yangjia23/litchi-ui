6b9c48643c034698e889dea543be99b702983aa2
yangjay <yj960203only@gmail.com>
2019-06-03 21:29:46 +0800

    feat(component): lc-button 组件增加 loading 效果

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,9 +1,10 @@
 <template>
   <button class="lc-button" :class="{[`icon-${iconPosition}`]: true}">
-    <lc-icon :name="icon" v-if="icon" class="icon"></lc-icon>
-    <div class="content">
+    <lc-icon v-if="loading" name="loading" class="lc-button__loading"></lc-icon>
+    <lc-icon :name="icon" v-if="!loading && icon" class="lc-button__icon"></lc-icon>
+    <span class="lc-button__content">
       <slot></slot>
-    </div>
+    </span>
   </button>
 </template>
 
@@ -20,12 +21,22 @@ export default {
       validator (value) {
         return ['left', 'right'].includes(value)
       }
-    }
+    },
+    loading: Boolean
   }
 };
 </script>
 
 <style lang="less" scoped>
+@keyframes spin {
+  0%{
+    transform: rotate(0deg)
+  }
+  100%{
+    transform: rotate(360deg)
+  }
+}
+
 .lc-button {
   font-size: var(--button-size);
   height: var(--button-height);
@@ -47,22 +58,26 @@ export default {
   &:focus {
     outline: none;
   }
-  > .icon {
-    order: 1;
-    margin-right: .3em;
+  &__loading{
+    margin-right: .2em;
+    animation: spin 1.5s infinite linear;
   }
-  > .content {
+  &__icon {
+    order: 1;
+    margin-right: .2em;
+  }
+  &__content {
     order: 2;
   }
 
   &.icon-right {
-    > .content {
+    .lc-button__content {
       order: 1;
     }
-    > .icon {
+    .lc-button__icon {
       order: 2;
       margin-right: 0;
-      margin-left: .3em;
+      margin-left: .2em;
     }
   }
 }
